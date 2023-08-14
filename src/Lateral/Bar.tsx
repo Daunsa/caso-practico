83cb76a8af18f6f13693876804a87f133598bb89
daunsa <dtapias@unsa.edu.pe>
2023-08-14 13:35:50 -0500

    proceso de listado de data hecho

--- src/Lateral/Bar.tsx
+++ src/Lateral/Bar.tsx
@@ -1,17 +1,59 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from './Card';
 import Loading from './Loading';
 import Failed from './Failed';
+import axios from 'axios';
 
 const Bar: React.FC = () => {
 
     const [state, setState] = useState(0);
+    const [data, setdata] = useState<any[]>([]);
+    const [nextPage, setNextPage] = useState(null);
+
+    const fetchCharacters = (url: string) => {
+        axios.get(url)
+            .then(response => {
+                const newData = response.data.results;
+                const newNextPage = response.data.next;
+                if (newNextPage === null) {
+                    setState(2);
+                }
+                console.log(newData)
+
+                setdata(prevData => [...prevData, ...newData]);
+                setNextPage(newNextPage);
+
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                setState(1);
+            });
+    };
+
+    useEffect(() => {
+        fetchCharacters('https://swapi.dev/api/people/');
+    }, []);
+
+    const handleScroll = (e: React.UIEvent<HTMLDivElement>) => {
+        const { scrollTop, scrollHeight, clientHeight } = e.currentTarget;
+        if (scrollTop + clientHeight === scrollHeight) {
+            if (nextPage) {
+                fetchCharacters(nextPage);
+            }
+        }
+    };
 
     return (
-        <div className="flex-grow overflow-y-auto bg-white w-96 border-r border-gray-400">
-            <Card />
-            <Loading />
-            <Failed />
+        <div onScroll={handleScroll} className="flex-grow overflow-y-auto bg-white w-96 border-r border-gray-400">
+            <Card data={data} />
+            {state === 0 ?
+                <Loading />
+                :
+                state === 1 ?
+                    <Failed />
+                    :
+                    <></>
+            }
         </div>
     );
 };
--- src/Lateral/Card.tsx
+++ src/Lateral/Card.tsx
@@ -1,31 +1,85 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const Card: React.FC = () => {
+interface SpeciesCache {
+    [key: string]: any;
+}
+
+interface PlanetCache {
+    [key: string]: any;
+}
+
+const Card: React.FC<{ data: any[] }> = ({ data }) => {
+
+    const [speciesCache, setSpeciesCache] = useState<SpeciesCache>({});
+    const [planetCache, setPlanetCache] = useState<PlanetCache>({});
+
+    useEffect(() => {
+        const fetchPlanet = async (planetUrl: string) => {
+            if (!planetCache[planetUrl]) {
+                const response = await axios.get(planetUrl);
+                const planetData = response.data;
+
+                setPlanetCache(prevCache => ({
+                    ...prevCache,
+                    [planetUrl]: planetData,
+                }));
+            }
+        };
+
+        const fetchSpecies = async (speciesUrl: string) => {
+            if (!speciesCache[speciesUrl]) {
+                const response = await axios.get(speciesUrl);
+                const speciesData = response.data;
+
+                setSpeciesCache(prevCache => ({
+                    ...prevCache,
+                    [speciesUrl]: speciesData,
+                }));
+            }
+        };
+
+        data.forEach(character => {
+            fetchPlanet(character.homeworld);
+            if (character.species.length > 0) {
+                fetchSpecies(character.species[0]);
+            }
+        });
+    }, [data, planetCache, speciesCache]);
 
     return (
-        <div className="pl-4">
-            <div className="border-b border-gray-400 py-4 flex">
-                <div className="flex-grow">
-                    <h2 className="text-xl font-semibold">perrito</h2>
-                    <p className="text-sm">perrito2</p>
-                </div>
-                <div className="flex-none flex items-center">
-                    <div className="flex items-center p-2">
-                        <svg
-                            className="w-6 h-6"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path
-                                d="M9.71069 18.2929C10.1012 18.6834 10.7344 18.6834 11.1249 18.2929L16.0123 13.4006C16.7927 12.6195 16.7924 11.3537 16.0117 10.5729L11.1213 5.68254C10.7308 5.29202 10.0976 5.29202 9.70708 5.68254C9.31655 6.07307 9.31655 6.70623 9.70708 7.09676L13.8927 11.2824C14.2833 11.6729 14.2833 12.3061 13.8927 12.6966L9.71069 16.8787C9.32016 17.2692 9.32016 17.9023 9.71069 18.2929Z"
-                                fill="#0F0F0F"
-                            />
-                        </svg>
+        <>
+            {data.map((character, index) => {
+                const speciesData = speciesCache[character.species[0]];
+                const planetData = planetCache[character.homeworld];
+                return (
+                    <div className="pl-4">
+                        <div key={index} className="border-b border-gray-400 py-4 flex">
+                            <div className="flex-grow">
+                                <h2 className="text-xl font-semibold">{character.name}</h2>
+                                <p className="text-sm">{speciesData ? speciesData.name : 'unknown'} from{' '}
+                                    {planetData ? planetData.name : 'Loading planet...'}</p>
+                            </div>
+                            <div className="flex-none flex items-center">
+                                <div className="flex items-center p-2">
+                                    <svg
+                                        className="w-6 h-6"
+                                        viewBox="0 0 24 24"
+                                        fill="none"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <path
+                                            d="M9.71069 18.2929C10.1012 18.6834 10.7344 18.6834 11.1249 18.2929L16.0123 13.4006C16.7927 12.6195 16.7924 11.3537 16.0117 10.5729L11.1213 5.68254C10.7308 5.29202 10.0976 5.29202 9.70708 5.68254C9.31655 6.07307 9.31655 6.70623 9.70708 7.09676L13.8927 11.2824C14.2833 11.6729 14.2833 12.3061 13.8927 12.6966L9.71069 16.8787C9.32016 17.2692 9.32016 17.9023 9.71069 18.2929Z"
+                                            fill="#0F0F0F"
+                                        />
+                                    </svg>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-        </div>
+                )
+            })}
+        </>
     );
 };
 
