c3ec6ca5e7a3461db20dc583a6e26f1d45417774
daunsa <dtapias@unsa.edu.pe>
2023-08-14 10:29:27 -0500

    navbar creado

---
+++ src/Navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className="bg-black py-4 pl-8">
+      <div className="text-white font-bold">
+        Ravn Star Wars Registry
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
